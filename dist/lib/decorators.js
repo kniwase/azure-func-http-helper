5fc87ca54824f359c6c346a0aa74ff41c9279f21
Kento Niwase <kento.niwase@asimov-robo.com>
2022-06-07 14:22:16 +0900

    build

--- dist/lib/decorators.js
+++ dist/lib/decorators.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AzureFuncContext = void 0;
 const common_1 = require("@nestjs/common");
-exports.AzureFuncContext = common_1.createParamDecorator((_data, ctx) => {
+exports.AzureFuncContext = (0, common_1.createParamDecorator)((_data, ctx) => {
     return ctx.switchToHttp().getRequest().context;
 })();
